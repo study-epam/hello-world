3b1de8ff708864cdbcc4c288f0ef2bf0430ba962
study-epam <g4595426@gmail.com>
2020-07-28 04:49:52 +0300

    Hello World

---
+++ src/LearnMain.java
@@ -0,0 +1,26 @@
+public class LearnMain {
+    public static void main(String[] args) {
+//        System.out.println("1.");
+//        System.out.println("2..");
+//        System.out.println("3...");
+//        System.out.println("Hello World!");
+
+//        int i = 1;
+//        System.out.println(i);
+//        i = i + 1;
+//        System.out.println(i);
+//        i = i + 1;
+//        System.out.println(i);
+
+        int a = 1;
+        int b = 2;
+//        int c = a + b;
+//        System.out.println("sum = " + c);
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
